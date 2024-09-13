4a8816c0550305f546c2f7ad375f2e60123c605e
Hrishabh3829 <2200032748@kluniversity.in>
2024-09-13 16:16:57 +0530

    Material-UI form controls

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,9 +1,41 @@
+import { FormControl, FormGroup, Input, InputLabel, styled } from "@mui/material"
+
+const css = styled(FormGroup)`
+    width: 50%;
+    margin: 50px auto;
+    & > div {
+        margin-bottom: 20px;
+       
+    }
+`
+
+
 const AddUser= () =>{
     return (
+
         
-        <p>
-        Hello from AddUser
-        </p>
+       <css>
+        <FormControl>
+            <InputLabel>Name</InputLabel>
+            <Input/>
+        </FormControl>
+
+        <FormControl>
+            <InputLabel>Username</InputLabel>
+            <Input/>
+        </FormControl>
+            
+        <FormControl>
+            <InputLabel>Email</InputLabel>
+            <Input/>
+        </FormControl>
+        
+        <FormControl>
+            <InputLabel>Phone</InputLabel>
+            <Input/>
+        </FormControl>
+        
+        </css>
     )
 
 }
